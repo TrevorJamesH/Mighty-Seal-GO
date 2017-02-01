ff5f6dd07b24b1c063dffcedced1750b2788dc30
TrevorJamesH <jetsamrover@gmail.com>
2017-02-01 15:52:49 -0800

    add feature: click buttons to place game pieces

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,54 +2,70 @@ import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css';
 
+
+
+class GameBoard {
+  constructor( size ){
+    this.size = size
+    this.board = []
+    this.initializeBoard()
+  }
+
+  initializeBoard(){
+    for(let x = 0; x < this.size; x++){
+      this.board[x] = []
+      for (let y = 0; y < this.size; y++){
+        this.board[x][y] = new Gamepeice(x,y)
+      }
+    }
+    return this.board
+  }
+}
+
+class Gamepeice {
+  constructor(x,y) {
+    this.x = x
+    this.y = y
+    this.value = 'blank'
+  }
+}
+
 class Board extends Component {
   constructor() {
     super();
-    this.size = 9;
     this.state = {
-
-      // wIsNext: true,
-      board: this.createGameBoard(this.size),
-
+      wIsNext: true,
+      gameBoard: new GameBoard(19),
     };
   }
 
-  createGameBoard(size){
-    let gameBoard = [];
-      for(let x = 0; x < size; x++){
-        gameBoard[x] = []
-        for (let y = 0; y < size; y++){
-          gameBoard[x][y] = ['Y:',y,',','X:',x]
-        }
+  handleClick(event,cell) {
+    console.log(cell)
+    let newState = this.state
+    let value = newState.gameBoard.board[cell.x][cell.y].value
+    if( value === 'blank' || value === 'white'){
+      newState.gameBoard.board[cell.x][cell.y].value = 'black'
+      this.setState(newState)
     }
-
-    return gameBoard
+    if( value === 'blank' || value === 'black'){
+      newState.gameBoard.board[cell.x][cell.y].value = 'white'
+      this.setState(newState)
+    }
   }
-
-  handleClick(x,y) {
-    const squares = this.state.squares.slice();
-
-    squares[x][y] = this.state.wIsNext ? 'W' : 'B';
-    this.setState({
-      squares: squares,
-      wIsNext: !this.state.wIsNext,
-    });
-  }
-
 
   render() {
-
-    let rows = this.state.board.map(function(row) {
+    let rows = this.state.gameBoard.board.map( (row,index) => {
       let cells = [];
       for (let i in row) {
-        cells.push(<button>{row[i]}</button>);
+        cells.push(<td key={`${row[i].x}-${row[i].y}`}>
+                    <button x = {row[i].x} y = {row[i].y} className={row[i].value} onClick={(event) => this.handleClick(event,row[i])}></button>
+                  </td>);
       }
-      return <tr>{cells}</tr>;
+      return <tr key={index}>{cells}</tr>;
     });
 
-
     return (
-      <table className="MyClassName">
+      <table className="board">
         <tbody>{rows}</tbody>
       </table>
     );
